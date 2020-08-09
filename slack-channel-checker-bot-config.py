0cc83cbea41a7c110fffea622a0fecfed1e85b8b
Masaki Miyata <catlover@38ta.com>
2020-08-09 20:09:02 +0900

    [clean]add some comment only

--- slack-channel-checker-bot-config.py
+++ slack-channel-checker-bot-config.py
@@ -13,5 +13,5 @@ tz_name            = "JST"
 # Not Required
 exclude_channel_id      = ["if needed, please input"] # When two or more, like this ["one", "tow"]
 exclude_users_id        = ["if needed, please input"] # When two or more, like this ["one", "tow"]
-exclude_message_subtype = ["if needed, please input"] # When two or more, like this ["channel_join", "channel_leave"]
+exclude_message_subtype = ["if needed, please input"] # When two or more, like this ["channel_join", "channel_leave", "bot_message"]
 # ------------------------------------------------------------------------------
